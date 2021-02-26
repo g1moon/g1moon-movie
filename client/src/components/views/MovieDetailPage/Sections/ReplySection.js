5b62cf364d80c4c07e4e20eba4ee4df4f1e83809
g1moon <gmoon.cho@gmail.com>
2021-02-26 19:37:52 +0900

    feat : 영화 상세보기에서 댓글 다는 기능 구현을 위해 대략적인 틀 추가

---
+++ client/src/components/views/MovieDetailPage/Sections/ReplySection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function ReplySection() {
+    return (
+        <div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            ReplySection
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+        </div>
+    );
+}
+
+export default ReplySection;
